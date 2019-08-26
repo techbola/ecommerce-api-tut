7944e5232ca9de637ba819e36e01134a4ebd5b49
techbola <bfagbowore@gmail.com>
2019-08-26 02:24:38 +0100

    Review Resource and Product Pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
     public function index()
     {
 
-        $products = Product::all();
+        $products = Product::paginate(20);
 
 //        return ProductResource::collection($products);
 
