54ad22dc773cfc03de667bec77b2fad5795719ba
techbola <bfagbowore@gmail.com>
2019-08-26 01:14:56 +0100

    Seeding For Product and Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+                            return \App\Model\Product::all()->random();
+                        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0, 5),
     ];
 });
