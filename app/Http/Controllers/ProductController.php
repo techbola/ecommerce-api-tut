57804f0ffd8928e2b89602f0a10c65a414609632
techbola <bfagbowore@gmail.com>
2019-08-26 03:39:30 +0100

    Destroy Product API done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -118,6 +118,9 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+
+        $product->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
